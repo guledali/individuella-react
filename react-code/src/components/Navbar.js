013e10f414e48fed407c4568832814fc11851131
guledali <guled.ali01@gmail.com>
2018-04-30 14:31:38 +0200

    Added react router dom and eslinter file!

--- react-code/src/components/Navbar.js
+++ react-code/src/components/Navbar.js
@@ -1,20 +1,38 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import styled from 'styled-components'
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
 
-class Navbar extends React.Component {
+const Navbar = props => (
+  <Router>
+    <div>
+      <Bar className="navbar navbar-light">
+        <Image>
+          <img src={props.Image} alt="hello" />
+          <Pills className="nav nav-pills">
+            <li className="nav-item">
+              <Abutton className="nav-link active font-weight-bold rounded-0">
+                <Linkcolor to="/about">About</Linkcolor>
+              </Abutton>
+            </li>
+          </Pills>
+        </Image>
+      </Bar>
+      <Route path="/about" component={About} />
+    </div>
+  </Router>
+);
 
-    render() {
-        return (
-            <Bar className="navbar navbar-light">
-                <Image>
-                    <img src={this.props.Image} alt="hello" />
-                </Image>
-            </Bar>
-
-        );
-    }
-}
+Navbar.propTypes = {
+  Image: PropTypes.string.isRequired,
+};
 
 export default Navbar;
 
@@ -30,3 +48,24 @@ const Image = styled.div`
    margin-left: 15px !important;
   }
 `;
+
+const Pills = styled.ul`
+  display: inline-flex !important;
+  margin-left: 10px;
+`;
+
+const Abutton = styled.div`
+    background #323330 !important;
+    color: #f5da55 !important;
+    padding: 1rem !important;
+    a:hover {
+        color: #0056b3;
+        text-decoration: none;
+    }
+ `;
+const Linkcolor = styled(Link)`
+    background #323330 !important;
+    color: #f5da55 !important;
+    padding: 1rem !important;
+    text-decoration: none;
+ `;
